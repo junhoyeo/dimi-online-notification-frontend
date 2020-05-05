4705023edf8dfdf1c9e6eb47f027541b37206be5
JunhoYeo <hanaro0704@gmail.com>
2020-05-06 03:47:19 +0900

    📌 Listen and notify messages

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,7 +2,25 @@ importScripts('https://www.gstatic.com/firebasejs/5.9.2/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/5.9.2/firebase-messaging.js');
 
 firebase.initializeApp({
+  apiKey: 'AIzaSyCvnGL_lxwdqxYsbmZiBjvzd23L-rTHwRs',
+  authDomain: 'dimi-online-notification.firebaseapp.com',
+  databaseURL: 'https://dimi-online-notification.firebaseio.com',
+  projectId: 'dimi-online-notification',
+  storageBucket: 'dimi-online-notification.appspot.com',
   messagingSenderId: '839786842905',
+  appId: '1:839786842905:web:44ef84a594135395ea9388',
 });
 
 const messaging = firebase.messaging()
+
+messaging.setBackgroundMessageHandler(({ data: { notification } }) => {
+  const { title, body }  = (() => {
+    if (typeof notification === 'string') {
+      return JSON.parse(notification);
+    }
+    return notification;
+  })();
+	return self.registration.showNotification(title, {
+    body,
+  });
+});
--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+console.log(firebaseConfig);
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+const messaging = firebase.messaging();
+
 const notify = async () => {
-  await initializeFirebase();
+  messaging
+  .requestPermission()
+  .then(() => messaging.getToken())
+  .then(async (token) => {
+    console.log(token);
+    // await fetch('/register', { method: 'post', body: token });
+  })
+  .catch((err) => console.log(err));
+
+  messaging.onMessage(({ data: { notification } }) => {
+    const { title, body }  = (() => {
+      if (typeof notification === 'string') {
+        return JSON.parse(notification);
+      }
+      return notification;
+    })();
+    return self.registration.showNotification(title, {
+      body,
+    });
+  });
 };
